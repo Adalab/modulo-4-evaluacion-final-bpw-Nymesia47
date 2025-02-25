130af35b734f1e4f9338a18b793c08cb6d5216eb
Gabriella Calvano <gabcalvano@gmail.com>
2025-02-25 17:14:49 +0100

    created connection to DB function

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 //imports
 const express = require('express');
 const cors = require('cors');
+const mysql = require("mysql2/promise");
 
 //crear el servidor
 const server = express();
+require("dotenv").config();
 
 // configurar el servidor
 server.use(cors());
 server.use(express.json());
+
+//funcion para conectarse a la BD
+async function getDBconnection() {
+  const connection = await mysql.createConnection({
+    host:"localhost",
+    user: process.env.USER_DB,
+    password: process.env.PASS_DB,
+    database: "expense_tracker"
+  });
+  await connection.connect();
+  return connection;
+}
 
 const PORT = 5005;
 server.listen(PORT, () => {
